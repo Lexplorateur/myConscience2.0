e3b9aea4115a03eefcff672fe5a7559d5e6c620e
Jordan GILLES <36922007+Lexplorateur@users.noreply.github.com>
2018-05-17 23:15:02 +0200

    Update firebase.credencials.ts

--- src/app/firebase.credencials.ts
+++ src/app/firebase.credencials.ts
@@ -1,9 +1,9 @@
 export const FIREBASE_CONFIG = {
   // Initialize Firebase
-    apiKey: "AIzaSyAj9bdvqNLbbX9fjA9Tmz7zrqYweVG5oes",
-    authDomain: "myconscience-f3110.firebaseapp.com",
-    databaseURL: "https://myconscience-f3110.firebaseio.com",
-    projectId: "myconscience-f3110",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "269220788713"
+    messagingSenderId: ""
 };
